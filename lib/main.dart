835763c90d2fd5b9afd5d700c284258a75695897
upendarpenmetcha <37012026+upendarpenmetcha@users.noreply.github.com>
2022-03-06 02:22:38 +0530

    Initialisation

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.black45,
+              Colors.deepPurple,
+              Colors.purpleAccent,
+            ],
+          )
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.person,size: 40,),
+              Text("Vikranth"),
+              Text(
+                'Hi This is Vikranth Application Developer',
+                style: TextStyle(fontSize: 30),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("View My Resume"),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.integration_instructions),
+                  Icon(Icons.integration_instructions),
+                  Icon(Icons.integration_instructions),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Contact ME"),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
